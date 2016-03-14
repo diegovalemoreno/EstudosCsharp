1b1c12f90496ed562b43bf7a434320d11d250613
Diego de Souza vale Moreno <diego.moreno@linx.com.br>
2016-03-14 12:56:22 -0300

    Inclusão da LnSecurity.

--- Util.Infra.Classes/Util.Infra.Classes/LnSecurityUtil.cs
+++ Util.Infra.Classes/Util.Infra.Classes/LnSecurityUtil.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LnSecurityLib;
-using System.Runtime.InteropServices;
 using Linx.NFCE.Services.Util.Util;
 
 namespace Linx.NFCE.Services.Util.Persistence
