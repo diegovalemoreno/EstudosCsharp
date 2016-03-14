3e7d4ad40aaf22ddf1fddb7d07301ff125e9e814
Diego de Souza vale Moreno <diego.moreno@linx.com.br>
2016-03-13 22:26:46 -0300

    Conexão com o MySQL e SQLite.

---
+++ MySQLConnection/MySQLConnection/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQLConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+            DataTable dt = new DataTable();
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection(@"Data Source=C:\Program Files (x86)\Linx Sistemas\Linx MIDe Client\Database\ClientDB.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = this.txtSelect.Text;
+
+            SQLiteDataAdapter da = new SQLiteDataAdapter(sql, m_dbConnection);
+            da.Fill(dt);
+            gvdDados.DataSource = dt.DefaultView;
+
+
+            //SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            //command.ExecuteNonQuery();
+
+            
+            m_dbConnection.Close();
+        }
+    }
+}
